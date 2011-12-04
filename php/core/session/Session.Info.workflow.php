67676b0eec86ce82713807f68f157c297093ee0f
Vibhaj Rajan <vibhaj8@gmail.com>
2011-12-04 20:42:37 +0530

    v0.0.8 : addition core.session workflows

---
+++ php/core/session/Session.Info.workflow.php
@@ -0,0 +1,68 @@
+<?php 
+require_once(SBSERVICE);
+
+/**
+ *	@class SessionInfoWorkflow
+ *	@desc Returns session owner email by ID
+ *
+ *	@param sessionid string Resource ID [memory]
+ *
+ *	@return email string Session owner [memory]
+ *
+ *	@author Vibhaj Rajan <vibhaj8@gmail.com>
+ *
+**/
+class SessionInfoWorkflow implements Service {
+	
+	/**
+	 *	@interface Service
+	**/
+	public function input(){
+		return array(
+			'required' => array('sessionid')
+		);
+	}
+	
+	/**
+	 *	@interface Service
+	**/
+	public function run($memory){
+		$memory['msg'] = 'Session information given successfully';
+		
+		$workflow = array(
+		array(
+			'service' => 'transpera.relation.delete.workflow',
+			'conn' => 'tsconn',
+			'relation' => '`sessions`',
+			'sqlcnd' => "where `expiry` < now()",
+			'errormsg' => 'Invalid Session ID',
+			'check' => false
+		),
+		array(
+			'service' => 'transpera.relation.unique.workflow',
+			'args' => array('sessionid'),
+			'conn' => 'tsconn',
+			'relation' => '`sessions`',
+			'sqlcnd' => "where `sessionid`='\${sessionid}'",
+			'escparam' => array('sessionid'),
+			'errormsg' => 'Invalid Session ID'
+		),
+		array(
+			'service' => 'cbcore.data.select.service',
+			'args' => array('result'),
+			'params' => array('result.0.email' => 'email')
+		));
+		
+		return Snowblozm::execute($workflow, $memory);
+	}
+	
+	/**
+	 *	@interface Service
+	**/
+	public function output(){
+		return array('email');
+	}
+	
+}
+
+?>
