19fa530d0b7278d2b6ed14922dd37c3d54991ca9
Vibhaj Rajan <vibhaj8@gmail.com>
2012-02-29 21:06:27 +0530

    v0.8.3 : added RhythmLink People person workflows

--- php/init.php
+++ php/init.php
@@ -86,7 +86,7 @@
 	**/
 	//define('PHPMAILER', CBROOT.'../../../libraries/phpmailer/PHPMailer.class.php');
 	//define('CBQUEUECONF', CBROOT. 'config.php');
-	define('CACHELITE', 'Cache/Lite.php');
-	define('CACHELITEOUTPUT', 'Cache/Lite/Output.php');
+	//define('CACHELITE', 'Cache/Lite.php');
+	//define('CACHELITEOUTPUT', 'Cache/Lite/Output.php');
 	
 ?>
